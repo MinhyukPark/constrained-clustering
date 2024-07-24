2f1b3e412d41eced5b762b75808cb2058bb776e2
Minhyuk Park <minpark815@gmail.com>
2024-07-24 16:50:42 -0500

    Recovering progress

---
+++ includes/improved_mincut_only.h
@@ -0,0 +1,179 @@
+#ifndef IMPROVED_MINCUT_ONLY_H
+#define IMPROVED_MINCUT_ONLY_H
+#include "constrained.h"
+
+
+class ImprovedMincutOnly : public ConstrainedClustering {
+    public:
+        ImprovedMincutOnly(std::string edgelist, std::string algorithm, double clustering_parameter, bool start_with_clustering, std::string existing_clustering, int num_processors, std::string output_file, std::string log_file, int log_level) : ConstrainedClustering(edgelist, algorithm, clustering_parameter, start_with_clustering, existing_clustering, num_processors, output_file, log_file, log_level) {
+        };
+        int main() override;
+
+        static inline std::vector<std::vector<int>> RunClusterOnPartition(const igraph_t* graph, std::string algorithm, int seed, double clustering_parameter, std::vector<int>& partition) {
+            std::vector<std::vector<int>> cluster_vectors;
+            std::map<int, std::vector<int>> cluster_map;
+            igraph_vector_int_t nodes_to_keep;
+            igraph_vector_int_t new_id_to_old_id_map;
+            igraph_vector_int_init(&new_id_to_old_id_map, partition.size());
+            igraph_vector_int_init(&nodes_to_keep, partition.size());
+            for(size_t i = 0; i < partition.size(); i ++) {
+                VECTOR(nodes_to_keep)[i] = partition[i];
+            }
+            igraph_t induced_subgraph;
+            igraph_induced_subgraph_map(graph, &induced_subgraph, igraph_vss_vector(&nodes_to_keep), IGRAPH_SUBGRAPH_CREATE_FROM_SCRATCH, NULL, &new_id_to_old_id_map);
+            std::map<int, int> partition_map  = ConstrainedClustering::GetCommunities("", algorithm, seed, clustering_parameter, &induced_subgraph);
+            ConstrainedClustering::RemoveInterClusterEdges(&induced_subgraph, partition_map);
+            std::vector<std::vector<int>> connected_components_vector = ConstrainedClustering::GetConnectedComponents(&induced_subgraph);
+            for(size_t i = 0; i < connected_components_vector.size(); i ++) {
+                std::vector<int> translated_cluster_vector;
+                for(size_t j = 0; j < connected_components_vector.at(i).size(); j ++) {
+                    int new_id = connected_components_vector.at(i).at(j);
+                    translated_cluster_vector.push_back(VECTOR(new_id_to_old_id_map)[new_id]);
+                }
+                cluster_vectors.push_back(translated_cluster_vector);
+            }
+            igraph_vector_int_destroy(&nodes_to_keep);
+            igraph_vector_int_destroy(&new_id_to_old_id_map);
+            igraph_destroy(&induced_subgraph);
+            return cluster_vectors;
+        }
+
+        static inline void MinCutOrClusterWorker(const igraph_t* graph, std::string algorithm, int seed, double clustering_parameter) {
+            while (true) {
+                std::unique_lock<std::mutex> to_be_mincut_lock{to_be_mincut_mutex};
+                std::vector<int> current_cluster = CM::to_be_mincut_clusters.front();
+                std::set<int> current_cluster_set(current_cluster.begin(), current_cluster.end());
+                CM::to_be_mincut_clusters.pop();
+                to_be_mincut_lock.unlock();
+                if(current_cluster.size() == 1 && current_cluster[0] == -1) {
+                    // done with work!
+                    return;
+                }
+                igraph_vector_int_t nodes_to_keep;
+                igraph_vector_int_t new_id_to_old_id_vector_map;
+                igraph_vector_int_init(&nodes_to_keep, current_cluster.size());
+                for(size_t i = 0; i < current_cluster.size(); i ++) {
+                    VECTOR(nodes_to_keep)[i] = current_cluster[i];
+                }
+                std::vector<int> in_partition;
+                std::vector<int> out_partition;
+                bool is_well_connected = false;
+                bool is_non_trivial_cut = false;
+                int edge_cut_size = -1;
+                igraph_t induced_subgraph;
+                int num_nodes_removed = 0;
+                // we do mincuts until it's a non trivial mincut or if the current cluster is already well connected (perhaps after some trivial mincuts)
+                igraph_vector_int_init(&new_id_to_old_id_vector_map, igraph_vector_int_size(&nodes_to_keep));
+                igraph_induced_subgraph_map(graph, &induced_subgraph, igraph_vss_vector(&nodes_to_keep), IGRAPH_SUBGRAPH_CREATE_FROM_SCRATCH, NULL, &new_id_to_old_id_vector_map);
+                std::map<int, int> new_id_to_old_id_map;
+                std::map<int, int> newnew_id_to_old_id_map;
+                std::map<int, int> old_id_to_new_id_map;
+                for(int i = 0; i < igraph_vector_int_size(&new_id_to_old_id_vector_map); i ++) {
+                    new_id_to_old_id_map[i] = VECTOR(new_id_to_old_id_vector_map)[i];
+                    old_id_to_new_id_map[VECTOR(new_id_to_old_id_vector_map)[i]] = i;
+                }
+                while(!(is_non_trivial_cut || is_well_connected)) {
+                    MinCutCustom mcc(&induced_subgraph);
+                    edge_cut_size = mcc.ComputeMinCut();
+                    std::vector<int> in_partition_candidate = mcc.GetInPartition();
+                    std::vector<int> out_partition_candidate = mcc.GetOutPartition();
+                    is_well_connected = ConstrainedClustering::IsWellConnected(in_partition_candidate, out_partition_candidate, edge_cut_size, &induced_subgraph);
+                    is_non_trivial_cut = in_partition_candidate.size() > 1 && out_partition_candidate.size() > 1;
+                    if(is_well_connected || is_non_trivial_cut) {
+                        in_partition = in_partition_candidate;
+                        out_partition = out_partition_candidate;
+                    } else {
+                        int node_to_remove = -1;
+                        if(in_partition_candidate.size() == 1) {
+                            igraph_vector_int_t newnew_id_to_new_id_map;
+                            igraph_vector_int_init(&newnew_id_to_new_id_map, out_partition_candidate.size());
+                            node_to_remove = in_partition_candidate.at(0);
+                            current_cluster_set.erase(new_id_to_old_id_map[node_to_remove]);
+                            igraph_delete_vertices_idx(&induced_subgraph, igraph_vss_1(node_to_remove), NULL, &newnew_id_to_new_id_map);
+                            for(int i = 0; i < igraph_vector_int_size(&newnew_id_to_new_id_map); i ++) {
+                                int newnew_id = i;
+                                int new_id = VECTOR(newnew_id_to_new_id_map)[newnew_id];
+                                int old_id = new_id_to_old_id_map[new_id];
+                                old_id_to_new_id_map[old_id] = newnew_id;
+                                newnew_id_to_old_id_map[newnew_id] = old_id;
+                            }
+                            new_id_to_old_id_map = newnew_id_to_old_id_map;
+                            igraph_vector_int_destroy(&newnew_id_to_new_id_map);
+                            num_nodes_removed ++;
+                        }
+                        if(out_partition_candidate.size() == 1) {
+                            igraph_vector_int_t newnew_id_to_new_id_map;
+                            igraph_vector_int_init(&newnew_id_to_new_id_map, out_partition_candidate.size());
+                            node_to_remove = out_partition_candidate.at(0);
+                            current_cluster_set.erase(new_id_to_old_id_map[node_to_remove]);
+                            igraph_delete_vertices_idx(&induced_subgraph, igraph_vss_1(node_to_remove), NULL, &newnew_id_to_new_id_map);
+                            for(int i = 0; i < igraph_vector_int_size(&newnew_id_to_new_id_map); i ++) {
+                                int newnew_id = i;
+                                int new_id = VECTOR(newnew_id_to_new_id_map)[newnew_id];
+                                int old_id = new_id_to_old_id_map[new_id];
+                                old_id_to_new_id_map[old_id] = newnew_id;
+                                newnew_id_to_old_id_map[newnew_id] = old_id;
+                            }
+                            new_id_to_old_id_map = newnew_id_to_old_id_map;
+                            igraph_vector_int_destroy(&newnew_id_to_new_id_map);
+                            num_nodes_removed ++;
+                        }
+                        if(igraph_vector_int_empty(&nodes_to_keep)) {
+                            // this cluster completely disintegrated somehow
+                            // i don't think it'll ever reach 1 before it reaches 2 and does 1 node in both partitions
+                            break;
+                        }
+                    }
+                }
+                if(is_well_connected) {
+                    /* std::cerr << "cluster size " << std::to_string(current_cluster.size()) <<  " well connected after performing " << std::to_string(num_nodes_removed) << " trivial mincuts " << std::endl; */
+                    current_cluster = std::vector(current_cluster_set.begin(), current_cluster_set.end());
+                    {
+                        std::lock_guard<std::mutex> done_being_clustered_guard(CM::done_being_clustered_mutex);
+                        CM::done_being_clustered_clusters.push(current_cluster);
+                    }
+                } else if(is_non_trivial_cut) {
+                    /* std::cerr << "cluster mincut into " << std::to_string(in_partition.size()) << ":" << std::to_string(out_partition.size()) << " after performing " << std::to_string(num_nodes_removed) << " trivial mincuts " << std::endl; */
+                    if(in_partition.size() > 1) {
+                        std::vector<std::vector<int>> in_clusters = CM::RunClusterOnPartition(&induced_subgraph, algorithm, seed, clustering_parameter, in_partition);
+                        for(size_t i = 0; i < in_clusters.size(); i ++) {
+                            std::vector<int> translated_in_clusters;
+                            for(size_t j = 0; j < in_clusters[i].size(); j ++) {
+                                translated_in_clusters.push_back(new_id_to_old_id_map[in_clusters[i][j]]);
+                            }
+                            {
+                                std::lock_guard<std::mutex> to_be_clustered_guard(CM::to_be_clustered_mutex);
+                                CM::to_be_clustered_clusters.push(translated_in_clusters);
+                            }
+                        }
+                    }
+                    if(out_partition.size() > 1) {
+                        std::vector<std::vector<int>> out_clusters = CM::RunClusterOnPartition(&induced_subgraph, algorithm, seed, clustering_parameter, out_partition);
+                        for(size_t i = 0; i < out_clusters.size(); i ++) {
+                            std::vector<int> translated_out_clusters;
+                            for(size_t j = 0; j < out_clusters[i].size(); j ++) {
+                                translated_out_clusters.push_back(new_id_to_old_id_map[out_clusters[i][j]]);
+                            }
+                            {
+                                std::lock_guard<std::mutex> to_be_clustered_guard(CM::to_be_clustered_mutex);
+                                CM::to_be_clustered_clusters.push(translated_out_clusters);
+                            }
+                        }
+                    }
+                }
+                igraph_vector_int_destroy(&nodes_to_keep);
+                igraph_vector_int_destroy(&new_id_to_old_id_vector_map);
+                igraph_destroy(&induced_subgraph);
+            }
+        }
+    private:
+        static inline std::mutex to_be_mincut_mutex;
+        static inline std::condition_variable to_be_mincut_condition_variable;
+        static inline std::queue<std::vector<int>> to_be_mincut_clusters;
+        static inline std::mutex to_be_clustered_mutex;
+        static inline std::queue<std::vector<int>> to_be_clustered_clusters;
+        static inline std::mutex done_being_clustered_mutex;
+        static inline std::queue<std::vector<int>> done_being_clustered_clusters;
+};
+
+#endif
---
+++ src/improved_minuct_only.cpp
@@ -0,0 +1,88 @@
+#include "cm.h"
+
+int CM::main() {
+    /* std::random_device rd; */
+    /* std::mt19937 rng{rd()}; */
+    /* std::uniform_int_distribution<int> uni(0, 100000); */
+    this->WriteToLogFile("Loading the initial graph" , Log::info);
+    FILE* edgelist_file = fopen(this->edgelist.c_str(), "r");
+    igraph_t graph;
+    igraph_read_graph_edgelist(&graph, edgelist_file, 0, false);
+    fclose(edgelist_file);
+    this->WriteToLogFile("Finished loading the initial graph" , Log::info);
+
+    /* int before_mincut_number_of_clusters = -1; */
+    int after_mincut_number_of_clusters = -2;
+    int iter_count = 0;
+
+    if(this->start_with_clustering) {
+        /* int seed = uni(rng); */
+        int seed = 0;
+        std::map<int, int> node_id_to_cluster_id_map = ConstrainedClustering::GetCommunities("", this->algorithm, seed, this->clustering_parameter, &graph);
+        ConstrainedClustering::RemoveInterClusterEdges(&graph, node_id_to_cluster_id_map);
+    } else if(this->existing_clustering != "") {
+        std::map<int, int> node_id_to_cluster_id_map = ConstrainedClustering::ReadCommunities(this->existing_clustering);
+        ConstrainedClustering::RemoveInterClusterEdges(&graph, node_id_to_cluster_id_map);
+    }
+
+    /** SECTION Get Connected Components START **/
+    std::vector<std::vector<int>> connected_components_vector = ConstrainedClustering::GetConnectedComponents(&graph);
+    // store the results into the queue that each thread pulls from
+    for(size_t i = 0; i < connected_components_vector.size(); i ++) {
+        CM::to_be_mincut_clusters.push(connected_components_vector[i]);
+    }
+    /** SECTION Get Connected Components END **/
+    int previous_done_being_clustered_size = 0;
+    while (true) {
+        this->WriteToLogFile("Iteration number: " + std::to_string(iter_count), Log::debug);
+        if(iter_count % 10 == 0) {
+            this->WriteToLogFile("Iteration number: " + std::to_string(iter_count), Log::info);
+        }
+
+        /** SECTION MinCutOnceAndCluster Each Connected Component START **/
+        this->WriteToLogFile(std::to_string(CM::to_be_mincut_clusters.size()) + " [connected components / clusters] to be mincut", Log::debug);
+        /* before_mincut_number_of_clusters = CM::to_be_mincut_clusters.size(); */
+        /* if a thread gets a cluster {-1}, then they know processing is done and they can stop working */
+        for(int i = 0; i < this->num_processors; i ++) {
+            CM::to_be_mincut_clusters.push({-1});
+        }
+        /* start the threads */
+        std::vector<std::thread> thread_vector;
+        for(int i = 0; i < this->num_processors; i ++) {
+            /* int seed = uni(rng); */
+            int seed = 0;
+            thread_vector.push_back(std::thread(CM::MinCutOrClusterWorker, &graph, this->algorithm, seed, this->clustering_parameter));
+        }
+        /* get the result back from threads */
+        /* the results from each thread gets stored in to_be_clustered_clusters */
+        for(size_t thread_index = 0; thread_index < thread_vector.size(); thread_index ++) {
+            thread_vector[thread_index].join();
+        }
+        this->WriteToLogFile(std::to_string(CM::to_be_clustered_clusters.size()) + " [connected components / clusters] to be clustered after a round of mincuts", Log::debug);
+        this->WriteToLogFile(std::to_string(CM::done_being_clustered_clusters.size() - previous_done_being_clustered_size) + " [connected components / clusters] were found to be well connected", Log::debug);
+        previous_done_being_clustered_size = CM::done_being_clustered_clusters.size();
+        /** SECTION MinCutOnceAndCluster Each Connected Component END **/
+
+        /** SECTION Check If All Clusters Are Well-Connected START **/
+        after_mincut_number_of_clusters = CM::to_be_clustered_clusters.size();
+        if(after_mincut_number_of_clusters == 0) {
+            this->WriteToLogFile("all clusters are well-connected", Log::info);
+            this->WriteToLogFile("Total number of iterations: " + std::to_string(iter_count + 1), Log::info);
+            break;
+        }
+        /** SECTION Check If All Clusters Are Well-Connected END **/
+
+        while(!CM::to_be_clustered_clusters.empty()) {
+            CM::to_be_mincut_clusters.push(CM::to_be_clustered_clusters.front());
+            CM::to_be_clustered_clusters.pop();
+        }
+
+        iter_count ++;
+    }
+
+    igraph_destroy(&graph);
+
+    this->WriteToLogFile("Writing output to: " + this->output_file, Log::info);
+    this->WriteClusterQueue(CM::done_being_clustered_clusters);
+    return 0;
+}
